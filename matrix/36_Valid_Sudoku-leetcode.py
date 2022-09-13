0090b62c661c28ef68bd60210652350da8cb9556
dhanush <dhanush@kali.kali>
2022-09-13 19:35:07 +0530

    add: matrix

--- matrix/36_Valid_Sudoku-leetcode.py
+++ matrix/36_Valid_Sudoku-leetcode.py
@@ -1,15 +1,13 @@
 #   https://leetcode.com/problems/valid-sudoku/
 
 
-
-
 from typing import *
 
 
 # class Solution:
 #     def isValidSudoku(self, board: List[List[str]]) -> bool:
 #         lookup = [0]*10
-        
+
 #         for row in range(9):
 #             for column in range(9):
 #                 if board[row][column] == ".":
@@ -42,43 +40,49 @@ from typing import *
 #                     else:
 #                         return False
 #             lookup = [0]*10
-        
+
 #         return True
 
 import collections
+
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         cols = collections.defaultdict(set)
         rows = collections.defaultdict(set)
-        squares = collections.defaultdict(set) # key = (r /3, c /3)
-        print(cols,rows,squares)
-        
+        squares = collections.defaultdict(set)  # key = (r /3, c /3)
+        print(cols, rows, squares)
+
         for r in range(9):
             for c in range(9):
                 if board[r][c] == ".":
                     continue
-                if (board[r][c] in rows[r] or
-                    board[r][c] in cols[c] or
-                    board[r][c] in squares[(r // 3, c // 3)]):
+                if (
+                    board[r][c] in rows[r]
+                    or board[r][c] in cols[c]
+                    or board[r][c] in squares[(r // 3, c // 3)]
+                ):
                     return False
                 cols[c].add(board[r][c])
                 rows[r].add(board[r][c])
                 squares[(r // 3, c // 3)].add(board[r][c])
-        print(cols,rows,squares)     
+        print(cols, rows, squares)
         return True
-        
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     obj = Solution()
-    board = [["5","3",".",".","7",".",".",".","."],
-             ["6",".",".","1","9","5",".",".","."],
-             [".","9","8",".",".",".",".","6","."],
-             ["8",".",".",".","6",".",".",".","3"],
-             ["4",".",".","8",".","3",".",".","1"],
-             ["7",".",".",".","2",".",".",".","6"],
-             [".","6",".",".",".",".","2","8","."],
-             [".",".",".","4","1","9",".",".","5"],
-             [".",".",".",".","8",".",".","7","9"]]
-    
+    board = [
+        ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+        ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+        [".", "9", "8", ".", ".", ".", ".", "6", "."],
+        ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+        ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+        ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+        [".", "6", ".", ".", ".", ".", "2", "8", "."],
+        [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+        [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+    ]
+
     ans = obj.isValidSudoku(board)
     print(ans)
